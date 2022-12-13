533c36104ee8f36c6e34f3a1da8611bf5ff0f5ae
iqbal suwandi <qbalsuwandi@gmail.com>
2022-12-14 01:08:08 +0700

    tugas 7 pembuatan navigasi bawah

---
+++ app/src/main/java/com/example/navigasi/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.navigasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.navigasi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        navController= Navigation.findNavController(this,R.id.activity_main_nav_host_fragment)
+        setupWithNavController(binding.bottomNavigationView,navController)
+    }
+}
